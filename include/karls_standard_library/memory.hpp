7c1607024a7d5049e0e9a3024145a2c7412b3c25
Karl Thilking <thilking.k@northeastern.edu>
2025-11-06 22:07:47 -0500

    added unique_ptr specialization for arrays

--- include/karls_standard_library/memory.hpp
+++ include/karls_standard_library/memory.hpp
@@ -1,6 +1,8 @@
 #ifndef KARLS_STANDARD_LIBRARY_MEMORY_HPP
 #define KARLS_STANDARD_LIBRARY_MEMORY_HPP
 
+#include "cstddef.hpp"
+
 namespace karls_standard_library {
   // primary default delete
   template<typename T>
@@ -47,6 +49,9 @@ namespace karls_standard_library {
     // default constructor
     unique_ptr() : m_ptr(nullptr) {}
 
+    // destructor
+    ~unique_ptr() noexcept { delete m_ptr; }
+
     // raw pointer construction
     explicit unique_ptr(T* ptr) : m_ptr(ptr) {}
 
@@ -57,14 +62,13 @@ namespace karls_standard_library {
     unique_ptr& operator=(const unique_ptr& other) = delete;
 
     // move constructor
-    unique_ptr(unique_ptr&& other) : m_ptr(other.release()) {}
+    unique_ptr(unique_ptr&& other) noexcept : m_ptr(other.release()) {}
 
     // move assignment operator
     unique_ptr& operator=(unique_ptr&& other) {
       if (this != &other) {
         delete m_ptr;
-        m_ptr = other.m_ptr;
-        other.m_ptr = nullptr;
+        m_ptr = other.release();
       }
       return *this;
     }
@@ -98,6 +102,105 @@ namespace karls_standard_library {
 
     // dereference operator
     T& operator*() const noexcept { return *m_ptr; }
+  };
+
+  template<typename T>
+  class unique_ptr<T[]> {
+  private:
+    T* m_ptr;
+  public:
+    // default constructor
+    constexpr unique_ptr() noexcept : m_ptr(nullptr) {}
+
+    // nullptr constructor
+    constexpr unique_ptr(nullptr_t) noexcept : m_ptr(nullptr) {}
+
+    // destructor 
+    ~unique_ptr() noexcept { delete[] m_ptr; }
+
+    // constructor with raw pointer
+    explicit unique_ptr(T* ptr) : m_ptr(ptr) {}
+
+    // copy constructor (delete)
+    unique_ptr(const unique_ptr& other) = delete;
+
+    // copy assignment (delete)
+    unique_ptr& operator=(const unique_ptr& other) = delete;
+
+    // move constructor
+    unique_ptr(unique_ptr&& other) noexcept : m_ptr(other.release()) {}
+
+    // move assignment operator
+    unique_ptr& operator=(unique_ptr&& other)
+    {
+      if (this != &other)
+      {
+        delete m_ptr;
+        m_ptr = other.release();
+      }
+      return *this;
+    }
+
+    // nullptr assignment
+    unique_ptr& operator=(nullptr_t np)
+    {
+      delete m_ptr;
+      m_ptr = nullptr;
+      return *this;
+    }
+
+    // release ownership of the underlying pointer
+    T* release() noexcept
+    {
+      T* old = m_ptr;
+      m_ptr = nullptr;
+      return old;
+    }
+
+    // reset the underlying pointer to a new pointer
+    void reset(T* new) noexcept
+    {
+      T* old = m_ptr;
+      m_ptr = new;
+      if (old) delete old;
+    }
+
+    // swap underlying pointer with other unique_ptr
+    void swap(unique_ptr& other) noexcept
+    {
+      T* temp = move(m_ptr);
+      m_ptr = move(other.m_ptr);
+      other.m_ptr = move(temp);
+    }
+
+    // return the underlying pointer
+    T* get() const noexcept
+    {
+      return m_ptr;
+    }
+
+    // return true if unique_ptr instance is managing an array
+    explicit operator bool() const noexcept
+    {
+      return m_ptr != nullptr;
+    }
+
+    // derefence the pointer
+    T& operator*() const noexcept
+    {
+      return *m_ptr;
+    }
+
+    // get raw pointer
+    T* operator->() const noexcept
+    {
+      return m_ptr;
+    }
+
+    T& operator[](size_t index) const
+    {
+      return m_ptr[index];
+    }
   };
 
   // make unique non member function
