96a4155159e04faa224e8887a041570677474e91
Karl Thilking <thilking.k@northeastern.edu>
2025-11-06 19:35:53 -0500

    added min, max comparison and addeed strcmp

---
+++ include/karls_standard_library/algorithm.hpp
@@ -0,0 +1,15 @@
+#ifndef KARLS_STANDARD_LIBRARY_ALGORITHM_HPP
+#define KARLS_STANDARD_LIBRARY_ALGORITHM_HPP
+
+namespace karls_standard_library {
+  template<typename T>
+  const T& min(const T& a, const T& b) {
+    return (a < b) ? a : b;
+  }
+  template<typename T>
+  const T& max(const T& lhs, const T& rhs) {
+    return (a > b) ? a : b;
+  }
+}
+
+#endif
--- include/karls_standard_library/cstring.hpp
+++ include/karls_standard_library/cstring.hpp
@@ -2,6 +2,7 @@
 #define KARLS_STANDARD_LIBRARY_CSTRING_HPP
 
 #include "cstddef.hpp"
+#include "algorithm.hpp"
 
 namespace karls_standard_library {
   // strlen method for constr char ptr
@@ -19,6 +20,26 @@ namespace karls_standard_library {
       d[i] = s[i];
     }
     return dest;
+  }
+
+  /*
+  If lhs appears before rhs in lexicographical order, return negative value
+  If lhs and rhs are equal, return 0
+  If lhs appears after rhs in lexicographical order, return positive value
+  */
+  int strcmp(const char* lhs, const char* rhs) {
+    if (!lhs || !rhs) {
+      if (lhs == rhs) return 0;
+      return (lhs) ? 1 : -1;
+    }
+    while (*lhs && *rhs && *lhs == *rhs) {
+      if (*lhs != *rhs) {
+        return static_cast<unsigned char>(*lhs) - static_cast<unsigned char>(*rhs);
+      }
+      ++lhs;
+      ++rhs;
+    }
+    return static_cast<unsigned char>(*lhs) - static_cast<unsigned char>(*rhs);
   }
 }
 
