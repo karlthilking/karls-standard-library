418dd67669ab6f922ba7f640cbf87c9f167d443b
Karl Thilking <thilking.k@northeastern.edu>
2025-11-11 12:15:47 -0500

    added tests for all string constructors and assignment operators

--- tests/test_string.cpp
+++ tests/test_string.cpp
@@ -11,24 +11,83 @@ class string_test : public testing::Test
 protected:
   string_test() = default;
   ~string_test() = default;
+
+  void SetUp() override
+  {
+    cstr_constructed = string("hello");
+    cstr_fill_constructed = string("hello123", 5);
+    fill_constructed = string(5, 'a');
+    init_list_constructed = string{'w', 'o', 'r', 'd'};
+
+    copy_dummy1 = string("copy test 1");
+    copy_dummy2 = string("copy test 2");
+
+    move_dummy1 = string("move test 1");
+    move_dummy2 = string("move test 2");
+  }
+
+  string default_constructed;
+  string cstr_constructed;
+  string cstr_fill_constructed;
+  string fill_constructed;
+  string init_list_constructed;
+
+  string copy_dummy1;
+  string copy_dummy2;
+
+  string move_dummy1;
+  string move_dummy2;
 };
 
 TEST_F(string_test, default_constructor)
 {
-  string s1;
-  EXPECT_TRUE(s1.empty());
-  EXPECT_EQ(s1.size(), 0);
-  EXPECT_THROW(s1.at(0), std::out_of_range);
+  EXPECT_TRUE(default_constructed.empty());
+  EXPECT_EQ(default_constructed.size(), 0);
+  EXPECT_EQ(default_constructed.capacity(), 0);
+}
+
+TEST_F(string_test, cstr_constructors)
+{
+  EXPECT_TRUE(!cstr_constructed.empty());
+  EXPECT_EQ(cstr_constructed.size(), 5);
+  EXPECT_GE(cstr_constructed.capacity(), 5);
+
+  EXPECT_TRUE(!cstr_fill_constructed.empty());
+  EXPECT_EQ(cstr_fill_constructed.size(), 5);
+  EXPECT_GE(cstr_fill_constructed.capacity(), 5);
+
+  EXPECT_TRUE(cstr_constructed == cstr_fill_constructed);
+  EXPECT_TRUE(cstr_constructed == "hello");
+}
+
+TEST_F(string_test, fill_constructor)
+{
+  EXPECT_EQ(fill_constructed.size(), 5);
+  EXPECT_TRUE(fill_constructed == "aaaaa");
+  EXPECT_EQ(fill_constructed.capacity(), 5);
+}
+
+TEST_F(string_test, copy_operations)
+{
+  string temp1(copy_dummy1);
+  EXPECT_EQ(temp1, copy_dummy1);
+  EXPECT_EQ(temp1, "copy test 1");
+
+  string temp2;
+  temp2 = copy_dummy2;
+  EXPECT_EQ(temp2, copy_dummy2);
+  EXPECT_EQ(temp2, "copy test 2");
 }
 
 TEST_F(string_test, move_operations)
 {
-  string s1("test");
-  string s2(move(s1));
-  EXPECT_EQ(s2, "test");
+  string temp1(move(move_dummy1));
+  EXPECT_EQ(temp1, "move test 1");
+  EXPECT_FALSE(temp1 == move_dummy1);
 
-  string s3("test2");
-  string s4;
-  s4 = move(s3);
-  EXPECT_EQ(s4, "test2");
+
+  string temp2;
+  temp2 = move(move_dummy2);
+  EXPECT_EQ(temp2, "move test 2");
+  EXPECT_FALSE(temp2 == move_dummy2);
 }
